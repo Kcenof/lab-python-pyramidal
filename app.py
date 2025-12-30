2721bd9b37f501517f0806c891aafe3d94f9a4c5
Skab Serhii <ymrfenix06@mgail.com>
2025-12-30 13:06:39 +0200

    Initial commit: Pyramidal number calculation program

---
+++ app.py
@@ -0,0 +1,28 @@
+def calculate_pyramidal_number(n):
+    
+    if n < 0:
+       
+        raise ValueError("Число n не може бути меншим за 0!")
+    
+    if n == 0:
+        return 0
+
+    pyramidal_sum = 0
+    current_triangular = 0
+    
+    
+    for i in range(1, n + 1):
+        current_triangular += i
+        pyramidal_sum += current_triangular
+        
+    return pyramidal_sum
+
+if __name__ == "__main__":
+    try:
+        user_input = int(input("Введіть значення n: "))
+        
+        result = calculate_pyramidal_number(user_input)
+        print(f"Пірамідальне число для n={user_input} дорівнює: {result}")
+        
+    except ValueError as e:
+        print(f"Помилка: {e}")
